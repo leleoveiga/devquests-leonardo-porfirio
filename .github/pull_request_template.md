9654b9df1e92251c3a239e76e94733353dcb85b9
Rodrigo Borges <rborges.soares@gmail.com>
2022-09-28 15:42:18 -0300

    Initial setup

---
+++ .github/pull_request_template.md
@@ -0,0 +1,14 @@
+ 
+### Description of the new feature or improvement
+ 
+ 
+### Checklist
+Put an ```x``` in the boxes that apply.
+- [] Implements all acceptance criteria
+- [] Does not contain commented code
+ 
+### Screenshots
+Place here screenshots of screens or components that have changed.
+| Screenshot 1 | Screenshot 2 |
+| ------ | ------ |
+| print  | print |
---
+++ quests/quest-2/GitHubApp/Screens/Components/EmptyView.swift
@@ -0,0 +1,76 @@
+//
+//  EmptyView.swift
+//  GitHubApp
+//
+//  Created by Rodrigo Borges on 01/08/22.
+//
+
+import Foundation
+import UIKit
+
+final class EmptyView: UIView {
+
+    private var stackView: UIStackView = {
+
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 32
+        return stackView
+    }()
+    
+    private var titleLabel: UILabel = {
+
+        let label = UILabel()
+        label.text = "No repositories found"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private var subtitleLabel: UILabel = {
+
+        let label = UILabel()
+        label.text = "Search for users to see their public repositories here!"
+        label.textColor = UIColor.darkGray
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+    
+    init() {
+        
+        super.init(frame: .zero)
+
+        self.configureSubviews()
+        self.configureConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureSubviews() {
+        
+        self.backgroundColor = .white
+
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(subtitleLabel)
+        addSubview(stackView)
+    }
+    
+    private func configureConstraints() {
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+        ])
+        
+    }
+    
+}
